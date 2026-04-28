05b7f2297f16e67e8d6da96352d0a06caead0237
Chrischuka13 <divineprovidence30@gmail.com>
2026-04-28 08:32:33 +0100

    Initial commit

---
+++ src/routes/Routes.tsx
@@ -0,0 +1,30 @@
+import AuthLayout from "../layouts/AuthLayout";
+import RootLayout from "../layouts/RootLayout";
+import SuspenseUi from "../components/ui/SuspenseUi";
+import { createBrowserRouter, RouterProvider, type RouteObject } from "react-router";
+
+import React, { Children, Component, lazy, Suspense } from 'react'
+
+const Routes = () => {
+    const routes = [
+        {
+            path: "/",
+            Component: RootLayout,
+            hydrateFallbackElement: <SuspenseUi/>,
+            children: [
+                {
+                    index: true,
+                    lazy: async () => {
+                        const { default: Component} = await import ("../pages/home/Home");
+                        return {Component}
+                    },
+                },
+            ]
+        },
+    ] satisfies RouteObject[];
+    const router = createBrowserRouter(routes);
+    return <RouterProvider router={router} />;
+
+}
+
+export default Routes
