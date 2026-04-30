d9cf81a96706fac2eb67dd84be29bbeae7cb8dea
Chrischuka13 <divineprovidence30@gmail.com>
2026-04-30 13:40:40 +0100

    added shadcnui

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,12 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import Routes from "./routes/Routes";
-import Footer from "./components/footer/Footer";
-import NavBar from "./components/nav/NavBar";
 
 
 const queryClient =  new QueryClient()
 function App() {
   return(
     <QueryClientProvider client={queryClient}>
-      <NavBar/>
       <Routes/>
-      <Footer/>
     </QueryClientProvider>
   )
 }
