7d277a4dcf37373d035c88658a5ff464b6af9f03
TOBY_WEB01 <akejutobi9@gmail.com>
2026-04-30 13:42:29 +0100

    updstes

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -1,59 +1,57 @@
 import AuthLayout from "../layouts/AuthLayout";
 import RootLayout from "../layouts/RootLayout";
 import SuspenseUi from "../components/ui/SuspenseUi";
-import { createBrowserRouter, RouterProvider, type RouteObject } from "react-router";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  type RouteObject,
+} from "react-router";
 import Login from "../pages/auth/Login";
 import SignUp from "../pages/auth/SignUp";
 import ResetPassword from "../pages/auth/ResetPassword";
 import ForgotPassword from "../pages/auth/ForgotPassword";
 
-
-
-
 const Routes = () => {
-    const routes = [
+  const routes = [
+    {
+      path: "/",
+      Component: RootLayout,
+      hydrateFallbackElement: <SuspenseUi />,
+      children: [
         {
-            path: "/",
-            Component: RootLayout,
-            hydrateFallbackElement: <SuspenseUi/>,
-            children: [
-                {
-                    index: true,
-                    lazy: async () => {
-                        const { default: Component} = await import ("../pages/home/Home");
-                        return {Component}
-                    },
-                },
-            ]
+          index: true,
+          lazy: async () => {
+            const { default: Component } = await import("../pages/home/Home");
+            return { Component };
+          },
         },
-          {
-            path: "auth",
-            Component: AuthLayout,
-            children: [
-                {
-                    path: "login",
-                    Component: Login
-                },
-                {
-                    path: "createAccount",
-                    Component: SignUp
-                },
-                  {
-                    path: "forgotPassword",
-                    Component: ForgotPassword
-                },
-                  {
-                    path: "resetPassword",
-                    Component: ResetPassword
-                }
+      ],
+    },
+    {
+      path: "auth",
+      Component: AuthLayout,
+      children: [
+        {
+          path: "login",
+          Component: Login,
+        },
+        {
+          path: "createAccount",
+          Component: SignUp,
+        },
+        {
+          path: "forgotPassword",
+          Component: ForgotPassword,
+        },
+        {
+          path: "resetPassword",
+          Component: ResetPassword,
+        },
+      ],
+    },
+  ] satisfies RouteObject[];
+  const router = createBrowserRouter(routes);
+  return <RouterProvider router={router} />;
+};
 
-
-            ]
-        }
-    ] satisfies RouteObject[];
-    const router = createBrowserRouter(routes);
-    return <RouterProvider router={router} />;
-
-}
-
-export default Routes
+export default Routes;
