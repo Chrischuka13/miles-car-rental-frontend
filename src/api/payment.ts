1d2f11ce9b2b856ea630a83d1c9a7dad1da73aad
Chrischuka13 <divineprovidence30@gmail.com>
2026-06-04 12:03:57 +0100

    feat: added credentials bypass

--- src/api/payment.ts
+++ src/api/payment.ts
@@ -3,7 +3,9 @@ import axiosClient from "@/lib/utils";
 
 export const initiatePayment = async (payload: PaystackPaymentData) => {
   try {
-    const response = await axiosClient.post("/payment/initialize", payload);
+    const response = await axiosClient.post("/payment/initialize", payload, {
+      withCredentials: true,
+    });
     return response.data;
   } catch (error) {
     console.error("Error initiating payment:", error);
@@ -14,7 +16,10 @@ export const initiatePayment = async (payload: PaystackPaymentData) => {
 export const verifyPayment = async (reference: string) => {
   try {
     const response = await axiosClient.get(
-      `/payment/verify?reference=${reference}`
+      `/payment/verify?reference=${reference}`,
+      {
+        withCredentials: true,
+      },
     );
     return response;
   } catch (error) {
