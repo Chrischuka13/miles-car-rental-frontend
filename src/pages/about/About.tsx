85fa54bce68993436d6ddd52006cd448c73e97db
Chrischuka13 <divineprovidence30@gmail.com>
2026-06-04 13:51:15 +0100

    feat: additional chages made

--- src/pages/about/About.tsx
+++ src/pages/about/About.tsx
@@ -8,7 +8,7 @@ import Buttons2 from "@/components/ui/Buttons2";
 import Buttons from "@/components/ui/Buttons";
 
 function About() {
-    const {user} = useAuth();
+  const { user } = useAuth();
   return (
     <main className="">
       <section
@@ -20,7 +20,6 @@ function About() {
         {/* Dark Overlay */}
         <div className="absolute inset-0 bg-black/55"></div>
 
-     
         <div className="relative z-10 text-center px-4 md:px-8 max-w-4xl mx-auto ">
           <h1 className="text-4xl md:text-6xl lg:text-7xl font-bold text-white leading-tight mt-5">
             About <span className="text-orange-500">Miles</span> Car Rental
@@ -34,14 +33,11 @@ function About() {
 
           <div className=" mt-10 flex flex-col md:flex-row gap-8 w-full justify-center items-center ">
             <Link to="/cars/carListing">
-              <Buttons2 text="Explore cars"/>
+              <Buttons2 text="Explore cars" />
             </Link>
-            
 
             <button className="w-full md:w-auto bg-white hover:bg-gray-100 transition-all duration-300 text-gray-900 px-5 py-2 rounded-full font-semibold flex items-center justify-center gap-3 shadow-lg cursor-pointer">
-              <span className="w-7 h-7 rounded-full border border-gray-400 flex items-center justify-center text-sm">
-                
-              </span>
+              <span className="w-7 h-7 rounded-full border border-gray-400 flex items-center justify-center text-sm"></span>
               Watch our story
             </button>
           </div>
@@ -109,13 +105,16 @@ function About() {
               open and the ride is yours.
             </p>
           </div>
-
           <section className="w-full bg-[#f7f7f7] py-16 ">
             <div className="grid gap-6 grid-cols-1 lg:grid-cols-2">
               <div className="bg-white rounded-2xl p-4 md:p-10">
                 <div className="flex flex-col gap-3 mb-6">
                   <div>
-                <img src="/Frame 114 (2).png" className="w-10 h-10 mb-3" alt="" />
+                    <img
+                      src="/Frame 114 (2).png"
+                      className="w-10 h-10 mb-3"
+                      alt=""
+                    />
                   </div>
                   <p className="text-gray-700 font-medium">Our Mission</p>
                 </div>
@@ -129,7 +128,11 @@ function About() {
               <div className="bg-DarkBlue rounded-2xl p-6 md:p-10">
                 <div className="flex flex-col gap-3 mb-6">
                   <div>
-                <img src="/Frame 114 (3).png" className="w-10 h-10 mb-3" alt="" />
+                    <img
+                      src="/Frame 114 (3).png"
+                      className="w-10 h-10 mb-3"
+                      alt=""
+                    />
                   </div>
                   <p className="text-orange-400 font-medium">Our Vision</p>
                 </div>
@@ -144,39 +147,44 @@ function About() {
         </div>
       </section>
 
-      <WhyChooseUs/>
+      <WhyChooseUs />
 
-      <OurService/>
-        
-      <Testimonial/>
+      <OurService />
 
-   <section className="py-12">
-      <div className="w-11/12 container mx-auto">
-        <div className="bg-DeepOrange flex flex-col justify-center items-center p-4 md:p-10 rounded-lg text-center md:text-start">
-          <h3 className="text-3xl md:text-5xl text-white mb-2 font-semibold">Ready to move smarter?</h3>
-          <p className="text-white text-xl mb-4">Book your next ride in minutes and experience a better way to travel. Your road, your rules</p>
+      <Testimonial />
 
-              {user? (
-                <button className="flex items-center justify-center px-6 bg-DarkBlue rounded-full p-2 cursor-pointer text-white">Book Now<img src="/arrow.png" alt="" className="w-6"/></button>
-                
-              ) : (
-            <div className="md:flex justify-center items-center gap-4">
-              <div className="flex justify-center gap-4">
-                <Link to='/auth/register'>
-                  <Buttons text="Get Started"/>
-                </Link>
-                  
-                <button className="bg-white rounded-full p-2 cursor-pointer px-4 font-semibold text-gray-800 hover:bg-gray-100">
-                  <Link to='/cars/carListing'>Talk to our team</Link>
-                </button>
+      <section className="py-12">
+        <div className="w-11/12 container mx-auto">
+          <div className="bg-DeepOrange flex flex-col justify-center items-center p-4 md:p-10 rounded-lg text-center md:text-start">
+            <h3 className="text-3xl md:text-5xl text-white mb-2 font-semibold">
+              Ready to move smarter?
+            </h3>
+            <p className="text-white text-xl mb-4">
+              Book your next ride in minutes and experience a better way to
+              travel. Your road, your rules
+            </p>
+
+            {user ? (
+              <button className="flex items-center justify-center px-6 bg-DarkBlue rounded-full p-2 cursor-pointer text-white">
+                Book Now
+                <img src="/arrow.png" alt="" className="w-6" />
+              </button>
+            ) : (
+              <div className="md:flex justify-center items-center gap-4">
+                <div className="flex justify-center gap-4">
+                  <Link to="/auth/register">
+                    <Buttons text="Get Started" />
+                  </Link>
+
+                  <button className="bg-white rounded-full p-2 cursor-pointer px-4 font-semibold text-gray-800 hover:bg-gray-100">
+                    <Link to="/cars/carListing">Talk to our team</Link>
+                  </button>
+                </div>
               </div>
-            </div>
-              )}
-
-
+            )}
+          </div>
         </div>
-      </div>
-    </section>
+      </section>
     </main>
   );
 }
