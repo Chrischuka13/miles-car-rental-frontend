05b3356bdf2ee8979f3e230bc2a6ac653ba5c272
Chrischuka13 <divineprovidence30@gmail.com>
2026-06-04 13:04:44 +0100

    gg

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -3,14 +3,14 @@ import { clsx, type ClassValue } from "clsx";
 import { toast } from "react-toastify";
 import { twMerge } from "tailwind-merge";
 
-const BASEURL = import.meta.env.VITE_BASE_URL || "";
+const BASEURL = import.meta.env.VITE_BASE_URL || "/api/v1";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
 const config: CreateAxiosDefaults = {
-  baseURL: `${BASEURL}/api/v1`,
+  baseURL: `${BASEURL}`,
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
